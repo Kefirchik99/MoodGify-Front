3fc10e86ac4d64cce62a4f7e76a79a61684aafbb
YY <92721458+Kefirchik99@users.noreply.github.com>
2024-11-02 12:32:59 -0900

    added + and v functionality,fontawesome (#6)
    * added + and v functionality,fontawesome
    
    * created routing
    
    * added context template

---
+++ src/Body/Calendar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Calendar = () => {
+    return (
+        <div>
+            <h1>Calendar Page</h1>
+            <p>This is where the calendar will be displayed.</p>
+        </div>
+    );
+};
+
+export default Calendar;
---
+++ src/Body/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings Page</h1>
+            <p>This is where the options will be displayed.</p>
+        </div>
+    );
+};
+
+export default Settings;
