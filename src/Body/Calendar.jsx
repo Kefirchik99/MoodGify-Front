d63208d7cc7d39f2f8316731057d2064cbe3663e
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-11-14 12:09:04 +0200

    post frequency chart

--- src/Body/Calendar.jsx
+++ src/Body/Calendar.jsx
@@ -3,8 +3,27 @@ import React from 'react';
 
 import PostCalendar from './PostCalendar';
 import MoodList from './MoodList';
+import PostFrequencyChart from './PostFrequencyChart';
+import ProgressBarWithLabel from './ProgressBarWithLabel';
 
 const Calendar = () => {
+
+    const postData = [
+        { day: 'Понедельник', count: 5 },
+        { day: 'Вторник', count: 3 },
+        { day: 'Среда', count: 4 },
+        { day: 'Четверг', count: 6 },
+        { day: 'Пятница', count: 7 },
+        { day: 'Суббота', count: 2 },
+        { day: 'Воскресенье', count: 1 }, ];
+    
+      
+      const progressData = [
+        { label: 'Mood 1', value: 70, maxValue: 100 },
+        { label: 'Mood 2', value: 45, maxValue: 100 },
+        { label: 'Mood 3', value: 90, maxValue: 100 },
+      ];
+      
     return (
         <>
         <div>
@@ -14,6 +33,8 @@ const Calendar = () => {
 
         <PostCalendar />
         <MoodList />
+
+        <PostFrequencyChart data={postData} />
 
         </>
     );
