352005ec16be52916c902d3b22f07dcd16c2e13f
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-11-14 11:12:48 +0200

    calendar

--- src/Body/Calendar.jsx
+++ src/Body/Calendar.jsx
@@ -1,12 +1,19 @@
 
 import React from 'react';
 
+import PostCalendar from './PostCalendar';
+
 const Calendar = () => {
     return (
+        <>
         <div>
             <h1>Calendar Page</h1>
             <p>This is where the calendar will be displayed.</p>
         </div>
+
+        <PostCalendar />
+
+        </>
     );
 };
 
